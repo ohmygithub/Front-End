e450c7873e7655000074c5588895632b93b56312
Xinwei Wang <xinweiwa@usc.edu>
2016-02-18 11:54:43 -0800

    no message

--- Project3/js/app.js
+++ Project3/js/app.js
@@ -8,7 +8,7 @@ var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = Math.floor(Math.random()*300);
+    this.speed = Math.floor(Math.random()*200);
 };
 
 // Update the enemy's position, required method for game
@@ -18,7 +18,7 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x >= 505) {
-        this.x = this.x - 101;
+        this.x = - 101;
     } else {
         this.x = this.x + this.speed * dt;
     }
