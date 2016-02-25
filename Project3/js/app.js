5ef58f5ea5d4feb8a2fc9e3337da9624b51e0f58
Xinwei Wang <xinweiwa@usc.edu>
2016-02-24 22:45:05 -0800

    no message

--- Project3/js/app.js
+++ Project3/js/app.js
@@ -1,18 +1,20 @@
 var game = {
     tileHeight : 101,
-    canHeight : 505
+    canHeight : 606,
+    canWidth : 505
 }
 // Enemies our player must avoid
 var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = Math.floor(Math.random()*200);
+    this.width = game.tileHeight;
+    this.height = game.tileHeight;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
@@ -25,6 +27,10 @@ Enemy.prototype.update = function(dt) {
         this.x = - game.tileHeight;
     } else {
         this.x = this.x + this.speed * dt;
+    }
+    if (isColliding(this)) {
+        player.x = 0;
+        player.y = 404;
     }
 };
 
@@ -42,15 +48,17 @@ var Player = function(x, y, direction) {
     this.x = x;
     this.y = y;
     this.direction = direction;
+    this.width = game.tileHeight;
+    this.height = game.tileHeight;
 };
 Player.prototype.update = function() {
     if (this.direction === 'l') {
         this.x = this.x - game.tileHeight;
-    } else if (this.direction === 'u') {
+    } else if (this.direction === 'd') {
         this.y = this.y + game.tileHeight;
     } else if (this.direction === 'r') {
-        this.x = this.x + game.tileHeight;
-    } else if (this.direction === 'd') {
+        this.x = this.x + game.tileHeight;        
+    } else if (this.direction === 'u') {
         this.y = this.y - game.tileHeight;
     } else {
     }
@@ -62,12 +70,11 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(input) {
     if (input === 'left' && this.x >= game.tileHeight) {
         this.direction = 'l';
-    } else if (input === 'up' && this.y <= game.canHeight - game.tileHeight) {
+    } else if (input === 'up' && this.y >= game.tileHeight) {
         this.direction = 'u';
-    } else if (input === 'right' && this.x <= game.canHeight - game.tileHeight) {
+    } else if (input === 'right' && this.x < game.canWidth - game.tileHeight) {
         this.direction = 'r';
-        console.log("direction is " + this.direction);
-    } else if (input === 'down' && this.y >= game.tileHeight) {
+    } else if (input === 'down' && this.y < game.canHeight - game.tileHeight * 2) {
         this.direction = 'd';
     } else {
         this.direction = 'nothing';
@@ -83,9 +90,13 @@ var allEnemies = [];
 for(var i = 0; i < 4; i++) {
     allEnemies.push(new Enemy(0, Math.floor(Math.random() * 3 + 1) * 101));
 }
-if(Enemy.x === player.x && Enemy.y === player.y) {
-    player.x = 0;
-    player.y = 404;
+
+function isColliding(enemy) {   
+    if (enemy.x < player.x + player.width && enemy.x + enemy.width > player.x && enemy.y < player.y + player.height && enemy.height + enemy.y > player.y) {
+        return true;
+    } else {
+        return false;
+    }   
 }
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
