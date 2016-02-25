c1f6f864901504ce2b7ed0c678b50055b7a2c7fa
Xinwei Wang <xinweiwa@usc.edu>
2016-02-25 01:44:56 -0800

    no message

--- Project3/js/app.js
+++ Project3/js/app.js
@@ -24,8 +24,6 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-        console.log(' y is ' + this.y);
-
     if (this.x >= game.canWidth * 2) {
         this.x = - game.tileWidth;
     } else {
@@ -35,8 +33,6 @@ Enemy.prototype.update = function(dt) {
         player.x = 0;
         player.y = 404;
     };
-        console.log(' yafter is ' + this.y);
-
 };
 
 // Draw the enemy on the screen, required method for game
