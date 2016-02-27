fa5e7737a4450bc221d3442a79359138d32db4b1
Xinwei Wang <xinweiwa@usc.edu>
2016-02-26 22:29:44 -0800

    no message

--- Project3/README.md
+++ Project3/README.md
@@ -4,3 +4,42 @@ frontend-nanodegree-arcade-game
 Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Frogger Game: Getting Started
+In this project you will be building your own arcade game. Once finished, your game should look as shown on this video:(https://www.youtube.com/watch?v=SxeHV1kt7iU&feature=youtu.be)
+
+Basic Functionality
+In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down. The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.
+
+
+Getting Started
+
+You won’t have to build the game from scratch. We have provided the art assets and game engine for you. You can download or clone them from our repository.
+The repository contains css, images, and js folders, as well as an index.html and a README.md file. Once you have downloaded the files we have provided, you will have to edit app.js to build the game.
+1.The css folder contains a style.css file which you do not need to edit
+2.The images folder contains the png image files, which are used when displaying the game. The images for the player and enemy character are going to be loaded from this folder.
+3.The js folder also contains the app engine needed to run the game and a resources.js file. You do not need to edit these files.
+4.index.html - opening index.html should load the game
+5.README.md should contain instructions on how to load and play the game (you will need to add those instructions).
+Inside the app.js file, you will need to implement the Player and the Enemy classes, using Object-Oriented JavaScript. Part of the code for the Enemy is provided to you, and you will need to complete the following:
+1.The Enemy function, which initiates the Enemy by:
+1)Loading the image by setting this.sprite to the appropriate image in the image folder (already provided)
+2)Setting the Enemy initial location (you need to implement)
+3)Setting the Enemy speed (you need to implement)
+2.The update method for the Enemy
+1)Updates the Enemy location (you need to implement)
+2)Handles collision with the Player (you need to implement)
+3.You can add your own Enemy methods as needed
+You will also need to implement the Player class, and you can use the Enemy class as an example on how to get started. At minimum you should implement the following:
+1.The Player function, which initiates the Player by:
+1)Loading the image by setting this.sprite to the appropriate image in the image folder (use the code from the Enemy function as an example on how to do that)
+2)Setting the Player initial location
+2.The update method for the Player (can be similar to the one for the Enemy)
+3.The render method for the Player (use the code from the render method for the Enemy)
+4.The handleInput method, which should receive user input, allowedKeys (the key which was pressed) and move the player according to that input. In particular:
+1)Left key should move the player to the left, right key to the right, up should move the player up and down should move the player down.
+2)Recall that the player cannot move off screen (so you will need to check for that and handle appropriately).
+3)If the player reaches the water the game should be reset by moving the player back to the initial location (you can write a separate reset Player method to handle that).
+5.You can add your own Player methods as needed.
+Once you have completed implementing the Player and Enemy, you should instantiate them by:
+6.Creating a new Player object
+7.Creating several new Enemies objects and placing them in an array called allEnemies
--- Project3/js/app.js
+++ Project3/js/app.js
@@ -1,10 +1,10 @@
 var game = {
-    numOfEnemies : 4,
-    speedIndex : 100,
-    tileWidth : 101,
-    tileHeight : 83,
-    canHeight : 606,
-    canWidth : 505
+    NUM_OF_ENEMIES : 4,
+    SPEED_INDEX : 100,
+    TILE_WIDTH : 101,
+    TILE_HEIGHT : 83,
+    CAN_HEIGHT : 606,
+    CAN_WIDTH : 505
 };
 // Enemies our player must avoid
 var Enemy = function (x, y) {
@@ -13,9 +13,9 @@ var Enemy = function (x, y) {
     'use strict';
     this.x = x;
     this.y = y;
-    this.speed = Math.floor((Math.random() + 1) * game.speedIndex);
-    this.width = game.tileWidth / 2;
-    this.height = game.tileHeight / 2;
+    this.speed = Math.floor((Math.random() + 1) * game.SPEED_INDEX);
+    this.width = game.TILE_WIDTH / 2;
+    this.height = game.TILE_HEIGHT / 2;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -28,18 +28,33 @@ Enemy.prototype.update = function (dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     'use strict';
-    if (this.x >= game.canWidth) {
-        this.x = -game.canWidth * 2;
+    if (this.x >= game.CAN_WIDTH) {
+        this.x = -game.CAN_HEIGHT * 2;
         createEnemy(1);
-    } else if (this.x >= -game.tileWidth) {
+    } else if (this.x >= -game.TILE_WIDTH) {
         this.x = this.x + this.speed * dt;
     }
-    if (isColliding(this)) {
+    if (this.isColliding()) {
         player.x = 0;
         player.y = 404;
     }
 };
-
+//Enemy.prototype.isColliding(enemy) {
+//    //'use strict';
+//    if (enemy.x < player.x + player.width && enemy.x + enemy.width > player.x && enemy.y < player.y + player.height && enemy.height + enemy.y > player.y) {
+//        return true;
+//    } else {
+//        return false;
+//    }
+//}
+Enemy.prototype.isColliding = function () {
+    'use strict';
+    if (this.x < player.x + player.width && this.x + this.width > player.x && this.y < player.y + player.height && this.height + this.y > player.y) {
+        return true;
+    } else {
+        return false;
+    }
+};
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function () {
     'use strict';
@@ -55,20 +70,20 @@ var Player = function (x, y, direction) {
     this.x = x;
     this.y = y;
     this.direction = direction;
-    this.width = game.tileWidth / 2;
-    this.height = game.tileHeight / 2;
+    this.width = game.TILE_WIDTH / 2;
+    this.height = game.TILE_HEIGHT / 2;
 };
 
 Player.prototype.update = function () {
     'use strict';
     if (this.direction === 'l') {
-        this.x = this.x - game.tileWidth;
+        this.x = this.x - game.TILE_WIDTH;
     } else if (this.direction === 'd') {
-        this.y = this.y + game.tileHeight;
+        this.y = this.y + game.TILE_HEIGHT;
     } else if (this.direction === 'r') {
-        this.x = this.x + game.tileWidth;
+        this.x = this.x + game.TILE_WIDTH;
     } else if (this.direction === 'u') {
-        this.y = this.y - game.tileHeight;
+        this.y = this.y - game.TILE_HEIGHT;
     }
     this.direction = 'nothing';
 };
@@ -78,13 +93,13 @@ Player.prototype.render = function () {
 };
 Player.prototype.handleInput = function (input) {
     'use strict';
-    if (input === 'left' && this.x >= game.tileWidth) {
+    if (input === 'left' && this.x >= game.TILE_WIDTH) {
         this.direction = 'l';
     } else if (input === 'up' && this.y > 0) {
         this.direction = 'u';
-    } else if (input === 'right' && this.x < game.canWidth - game.tileWidth) {
+    } else if (input === 'right' && this.x < game.CAN_WIDTH - game.TILE_WIDTH) {
         this.direction = 'r';
-    } else if (input === 'down' && this.y < game.canHeight - game.tileHeight * 3) {
+    } else if (input === 'down' && this.y < game.CAN_HEIGHT - game.TILE_HEIGHT * 3) {
         this.direction = 'd';
     } else {
         this.direction = 'nothing';
@@ -100,19 +115,12 @@ var allEnemies = [];
 var createEnemy = function (num) {
     'use strict';
     var i;
-    for (i = 0; i < num; i+=1) {
-        allEnemies.push(new Enemy(-game.tileWidth, Math.floor(Math.random() * 3 + 1) * game.tileHeight - 22));
+    for (i = 0; i < num; i += 1) {
+        allEnemies.push(new Enemy(-game.TILE_WIDTH, Math.floor(Math.random() * 3 + 1) * game.TILE_HEIGHT - 22));
     }
 };
-createEnemy(game.numOfEnemies);
-function isColliding(enemy) {
-    'use strict';
-    if (enemy.x < player.x + player.width && enemy.x + enemy.width > player.x && enemy.y < player.y + player.height && enemy.height + enemy.y > player.y) {
-        return true;
-    } else {
-        return false;
-    }
-}
+createEnemy(game.NUM_OF_ENEMIES);
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
