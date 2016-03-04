a8840ef59b2dbf9cd372b2703f845d4193e525ce
Xinwei Wang <xinweiwa@usc.edu>
2016-03-04 03:11:05 -0800

    no message

--- Project4/views/js/main.js
+++ Project4/views/js/main.js
@@ -451,19 +451,14 @@ var resizePizzas = function(size) {
     
     
     
-    
-    
-    
-    
-    
-    
+  var pizzaContainer = document.querySelectorAll(".randomPizzaContainer");
+  var dx = determineDx(pizzaContainer[0], size);
+  var newwidth = (document.querySelectorAll(".randomPizzaContainer")[0].offsetWidth + dx) + 'px';
     
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    for (var i = 0; i < pizzaContainer.length; i++) {
+      pizzaContainer[i].style.width = newwidth;
     }
   }
 
@@ -513,8 +508,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var scrollT = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrollT + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
