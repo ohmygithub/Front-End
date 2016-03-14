ba18135060fbeedcfd91eb8369f6720367858001
Xinwei Wang <xinweiwa@usc.edu>
2016-03-13 23:45:16 -0700

    add yelp api

--- Project5/js/app.js
+++ Project5/js/app.js
@@ -1,6 +1,7 @@
-var restaurantModel = function (eat) {
+var restaurantModel = function (eat, businessId) {
     'use strict';
 	var self = this;
+    var url = '';
 	self.name = eat.name;
 	self.position = eat.latLng;
     self.address = eat.address;
@@ -38,11 +39,11 @@ var restaurantModel = function (eat) {
 	// Initialize infowindow of the place
 	self.createInfowindow = function () {
 		self.infowindow = new google.maps.InfoWindow();
-		self.infowindow.setContent('<h3>' + self.name + '</h3>' + self.address);
 	};
 
 	// Add infowindow to the view
 	self.addInfowindow = function () {
+        self.infowindow.setContent('<h3>' + self.name + '</h3>' + self.address + '<hr/><a href=' + url + ' />' + url + '</a>');
 		self.infowindow.open(myMap, self.marker);
 
 		// Set the marker and info window to the center
@@ -52,18 +53,66 @@ var restaurantModel = function (eat) {
 	self.closeInfowindow = function () {
 		self.infowindow.close();
 	};
+    
+    self.yelp = function(businessId, marker) {
+            var auth = {
+                consumerKey: "oZsD8h9BM0VQtveN7sYvHg",
+                consumerSecret: "EQhmDrmyjsJH7F5s3SEe516JbGY",
+                accessToken: "N-R-C5cfJXo-4LrgGcpBJUQ6w3Y2_I6_",
+                accessTokenSecret: "H9H8IFqCUxHqXxqJuoM0dEtHiDo",
+                serviceProvider: {
+                    signatureMethod: "HMAC-SHA1"
+                }
+            };
+            var yelp_url = 'https://api.yelp.com/v2/business/' + businessId;
 
+            var parameters = {
+                oauth_consumer_key: auth.consumerKey,
+                oauth_token: auth.accessToken,
+                oauth_nonce: nonce_generate(),
+                oauth_timestamp: Math.floor(Date.now() / 1000),
+                oauth_signature_method: 'HMAC-SHA1',
+                oauth_version: '1.0',
+                callback: 'cb' // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+            };
+
+            var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, auth.consumerSecret, auth.accessTokenSecret);
+            parameters.oauth_signature = encodedSignature;
+
+            var errorTimeout = setTimeout(function() {
+                alert("Something went wrong");
+            }, 8000);
+
+            $.ajax({
+                url: yelp_url,
+                data: parameters,
+                cache: true, 
+                dataType: 'jsonp',
+                success: function(results) {
+                    clearTimeout(errorTimeout);
+                    url = results.url;
+                    self.addInfowindow();
+                },
+                fail: function() {
+                    alert("Problem occured!");
+                }
+            });
+        };
+    
+    
+    
 	// Change view to selected place
 	self.select = function () {
-		if (appView.currentLoc() !== undefined) {
-			appView.currentLoc().closeInfowindow();
-			appView.currentLoc().selected(false);
+
+        if (self !== undefined) {
+			self.closeInfowindow();
+			self.selected(false);
 		}
 
-		appView.currentLoc(self);
 		self.selected(true);
-        appView.currentLoc().animation();
-		appView.currentLoc().addInfowindow();
+        self.animation();
+        self.yelp(businessId, self);
+
 	};
 
     self.animation = function () {
@@ -72,6 +121,8 @@ var restaurantModel = function (eat) {
 
 	self.init();
 };
+
+
 
 function toggleBounce(obj) 
 {
@@ -82,11 +133,24 @@ function toggleBounce(obj)
     }
 }
 
+function nonce_generate(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
 // Initialize google map
 var myMap,
     marker;
 function mapInit() {
     'use strict';
+    
+    var appView = new ViewModel();
+    ko.applyBindings(appView);
+    
 	var initCenter = new google.maps.LatLng(34.066404, -118.296574);
 	var mapOpts = {
 		center: initCenter,
@@ -94,6 +158,7 @@ function mapInit() {
 	};
 	myMap = new google.maps.Map(document.getElementById('map'), mapOpts);
     
+    appView.init();
 }
 
 var ViewModel = function () {
@@ -105,18 +170,17 @@ var ViewModel = function () {
 
 	// neighbourhood restaurants
 	self.dining = ko.observableArray([
-    new restaurantModel({name: Restaurants.restaurants[0].name, address: Restaurants.restaurants[0].address, latLng: new google.maps.LatLng(Restaurants.restaurants[0].lat, Restaurants.restaurants[0].lng)}),
-    new restaurantModel({name: Restaurants.restaurants[1].name, address: Restaurants.restaurants[1].address, latLng: new google.maps.LatLng(Restaurants.restaurants[1].lat, Restaurants.restaurants[1].lng)}),
-    new restaurantModel({name: Restaurants.restaurants[2].name, address: Restaurants.restaurants[2].address, latLng: new google.maps.LatLng(Restaurants.restaurants[2].lat, Restaurants.restaurants[2].lng)}),
-    new restaurantModel({name: Restaurants.restaurants[3].name, address: Restaurants.restaurants[3].address, latLng: new google.maps.LatLng(Restaurants.restaurants[3].lat, Restaurants.restaurants[3].lng)}),
-    new restaurantModel({name: Restaurants.restaurants[4].name, address: Restaurants.restaurants[4].address, latLng: new google.maps.LatLng(Restaurants.restaurants[4].lat, Restaurants.restaurants[4].lng)})
+    new restaurantModel({name: Restaurants.restaurants[0].name, address: Restaurants.restaurants[0].address, latLng: new google.maps.LatLng(Restaurants.restaurants[0].lat, Restaurants.restaurants[0].lng)}, Restaurants.restaurants[0].businessId),
+    new restaurantModel({name: Restaurants.restaurants[1].name, address: Restaurants.restaurants[1].address, latLng: new google.maps.LatLng(Restaurants.restaurants[1].lat, Restaurants.restaurants[1].lng)}, Restaurants.restaurants[1].businessId),
+    new restaurantModel({name: Restaurants.restaurants[2].name, address: Restaurants.restaurants[2].address, latLng: new google.maps.LatLng(Restaurants.restaurants[2].lat, Restaurants.restaurants[2].lng)}, Restaurants.restaurants[2].businessId),
+    new restaurantModel({name: Restaurants.restaurants[3].name, address: Restaurants.restaurants[3].address, latLng: new google.maps.LatLng(Restaurants.restaurants[3].lat, Restaurants.restaurants[3].lng)}, Restaurants.restaurants[3].businessId),
+    new restaurantModel({name: Restaurants.restaurants[4].name, address: Restaurants.restaurants[4].address, latLng: new google.maps.LatLng(Restaurants.restaurants[4].lat, Restaurants.restaurants[4].lng)}, Restaurants.restaurants[4].businessId)
     ]);
 
 	// Search and filter
 	self.search = function (data) {
 		var filter = data();
 		self.filterList.removeAll();
-		self.removeMarkerAll();
 
 		// Display all restaurants when no input in search box
 		if (!filter) {filter = ""; }
@@ -140,12 +204,3 @@ var ViewModel = function () {
 		self.search(self.filterText);
 	};
 };
-
-// Global variable to communicate with the ViewModel
-var appView;
-// Initialize the map when the document is ready
-$(document).ready(function() {
-  appView = new ViewModel();
-  ko.applyBindings(appView);
-  appView.init();
-});
--- Project5/js/data.js
+++ Project5/js/data.js
@@ -5,30 +5,35 @@ var Restaurants = {
         {
             name : 'Mountain Cafe',
             address : '3064 w 8th st, Los Angeles, CA 90005',
+            businessId : 'mountain-cafe-los-angeles-3',
             lat : 34.057382,
             lng : -118.293674
         },
         {
             name : 'Sun Dong Dan',
             address : '3470 w 6th st #7, Los Angeles, CA 90020',
+            businessId : 'sun-nong-dan-los-angeles',
             lat : 34.062947,
             lng : -118.297450
         },
         {
             name : 'Feng Mao 2',
             address : '414 s Western Ave, Los Angeles, CA 90020',
+            businessId : 'feng-mao-lamb-kebab-los-angeles',
             lat : 34.066660,
             lng : -118.308755
         },
         {
             name : 'Buil Sam Gye Tang',
             address : '4204 w 3rd st, Los Angeles, CA 90020',
+            businessId : 'buil-samgye-tang-los-angeles',
             lat : 34.068763,
             lng : -118.305696
         },
         {
             name : 'Bak Kung2 Korean B.B.Q',
             address : '233 s Vermont Ave, Los Angeles, CA 90004',
+            businessId : 'bak-kung-korean-bbq-los-angeles-2',
             lat : 34.069506,
             lng : -118.291933
         }
